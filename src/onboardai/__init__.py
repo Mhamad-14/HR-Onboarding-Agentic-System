3ef461eb80d77361dae5228dd8f42067c3a05fa8
NorahS-40 <ns3271585@gmail.com>
2026-08-13 01:03:34 +0300

    Delete src/onboardai directory

--- src/onboardai/__init__.py
+++
@@ -1,7 +0,0 @@
-"""OnboardAI capstone package."""
-
-from .schemas import OnboardingRequest, SupervisorDecision, WorkerResult
-
-__all__ = ["OnboardingRequest", "SupervisorDecision", "WorkerResult"]
-
-__version__ = "0.1.0"
